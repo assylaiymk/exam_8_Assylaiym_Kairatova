4fc5d6b9a4ca3ae5113dbff5affba52d71336215
Assylaiym Kairatova <assylaiym.kairatova@gmail.com>
2022-10-29 17:53:50 +0600

    Добавлены validators в ratings

--- webapp/models/reviews.py
+++ webapp/models/reviews.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 from webapp.models.products import Product
 
 
@@ -8,7 +8,11 @@ class Review(models.Model):
     author = models.ForeignKey(to=User, verbose_name='Author', null=False, related_name='author', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='products', blank=True, on_delete=models.CASCADE)
     text = models.TextField(verbose_name='Text', max_length=3000, null=False, blank=False)
-    rating = models.FloatField(verbose_name='Rating', blank=False)
+    rating = models.FloatField(verbose_name='Rating', blank=False,
+                               validators=[
+                                            MaxValueValidator(5),
+                                            MinValueValidator(1)
+                                        ])
 
     def __str__(self):
         return f'{self.author} - {self.product} -  {self.text} - {self.rating}'
